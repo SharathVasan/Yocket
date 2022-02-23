a83f9970b4bd0e089ce27dc1e7fb7d94e5a52fa9
Sharath Vasan <75443379+SharathVasan@users.noreply.github.com>
2022-02-23 21:35:22 +0530

    Create 3a.js

---
+++ 3a.js
@@ -0,0 +1,50 @@
+const pg = require('pg')
+const pool = new pg.Pool({
+   user: process.env.user,
+host: process.env.host,
+database: process.env.database,
+password: process.env.password,
+port: process.env.port,
+});
+
+
+async function query (event) {
+  const client = await pool.connect(); 
+  let res, statement;
+    statement =`Select * from ${schema}.event`;
+  try {
+    try {
+      res = await client.query(statement);
+        res = res.rows;
+    // await client.query('COMMIT')
+    } catch (err) {
+    //  await client.query('ROLLBACK')
+      throw err;
+    }
+  } finally {
+    client.release();
+  }
+  return res;
+}
+
+exports.handler = async (event, context, callback) => {
+   schema = event.schema;  
+  var response ={};
+    try {
+      const rows = await query(event);
+      
+      //console.log(JSON.stringify(rows[0]))
+     response = {
+          "statusCode": 200,
+           "body":rows
+      };
+      
+      context.succeed(response);
+    } catch (err) {
+      response = {
+          "statusCode": 400,
+           "msg":err
+      };
+      context.succeed('Database ' + err);
+    }
+};
